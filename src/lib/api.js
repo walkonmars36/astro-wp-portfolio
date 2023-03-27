b70f1a0951620ffd0b6003f42213e300103b6949
Mark Lawson <81777919+walkonmars36@users.noreply.github.com>
2023-03-27 21:53:20 +0100

    Initial commit

---
+++ src/lib/api.js
@@ -0,0 +1,184 @@
+export async function navQuery(){
+    const siteNavQueryRes = await fetch(import.meta.env.WORDPRESS_API_URL, {
+        method: 'post', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            query: `{
+                menus(where: {location: MENU_1}) {
+                  nodes {
+                    name
+                    menuItems {
+                        nodes {
+                            uri
+                            url
+                            order
+                            label
+                        }
+                    }
+                  }
+                }
+                generalSettings {
+                    title
+                    url
+                    description
+                }
+            }
+            `
+        })
+    });
+    const{ data } = await siteNavQueryRes.json();
+    return data;
+}
+
+export async function homePagePostsQuery(){
+    const response = await fetch(import.meta.env.WORDPRESS_API_URL, {
+        method: 'post', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            query: `{
+                posts {
+                  nodes {
+                    date
+                    uri
+                    title
+                    commentCount
+                    excerpt
+                    categories {
+                      nodes {
+                        name
+                        uri
+                      }
+                    }
+                    featuredImage {
+                      node {
+                        mediaItemUrl
+                        altText
+                      }
+                    }
+                  }
+                }
+              }
+            `
+        })
+    });
+    const{ data } = await response.json();
+    return data;
+}
+
+
+export async function getNodeByURI(uri){
+    const response = await fetch(import.meta.env.WORDPRESS_API_URL, {
+        method: 'post', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            query: `query GetNodeByURI($uri: String!) {
+                nodeByUri(uri: $uri) {
+                  __typename
+                  isContentNode
+                  isTermNode
+                  ... on Post {
+                    id
+                    title
+                    date
+                    uri
+                    excerpt
+                    content
+                    categories {
+                      nodes {
+                        name
+                        uri
+                      }
+                    }
+                    featuredImage {
+                      node {
+                        mediaItemUrl
+                        altText
+                      }
+                    }
+                  }
+                  ... on Page {
+                    id
+                    title
+                    uri
+                    date
+                    content
+                  }
+                  ... on Category {
+                    id
+                    name
+                    posts {
+                      nodes {
+                        date
+                        title
+                        excerpt
+                        uri
+                        categories {
+                          nodes {
+                            name
+                            uri
+                          }
+                        }
+                        featuredImage {
+                          node {
+                            altText
+                            mediaItemUrl
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            variables: {
+                uri: uri
+            }
+        })
+    });
+    const{ data } = await response.json();
+    return data;
+}
+
+export async function getAllUris(){
+  const response = await fetch(import.meta.env.WORDPRESS_API_URL, {
+      method: 'post', 
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+          query: `query GetAllUris {
+            terms {
+              nodes {
+                uri
+              }
+            }
+            posts(first: 100) {
+              nodes {
+                uri
+              }
+            }
+            pages(first: 100) {
+              nodes {
+                uri
+              }
+            }
+          }
+          `
+      })
+  });
+  const{ data } = await response.json();
+  const uris = Object.values(data)
+    .reduce(function(acc, currentValue){
+      return acc.concat(currentValue.nodes)
+    }, [])
+    .filter(node => node.uri !== null)
+    .map(node => {
+      let trimmedURI = node.uri.substring(1);
+      trimmedURI = trimmedURI.substring(0, trimmedURI.length - 1)
+      return {params: {
+        uri: trimmedURI
+      }}
+    })
+
+  return uris;
+
+}
+
